4e0e903fc0a529164519cec2627fcfc04fcfce5c
RobertJacksonIO <jackson.t.robert@gmail.com>
2021-08-15 19:00:47 -0600

    Adding go.mod and go.sum.

--- example/ApiKeyExample.go
+++ example/ApiKeyExample.go
@@ -1,21 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
 
-	"github.com/fabioberger/coinbase-go"
-)
+// 	"github.com/fabioberger/coinbase-go"
+// )
 
-func main() {
-	// Instantiate a ApiKeyClient with key and secret set at environment variables
-	c := coinbase.ApiKeyClient(os.Getenv("COINBASE_KEY"), os.Getenv("COINBASE_SECRET"))
+// func main() {
+// 	// Instantiate a ApiKeyClient with key and secret set at environment variables
+// 	c := coinbase.ApiKeyClient(os.Getenv("COINBASE_KEY"), os.Getenv("COINBASE_SECRET"))
 
-	balance, err := c.GetBalance()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Balance is %f BTC", balance)
+// 	balance, err := c.GetBalance()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Printf("Balance is %f BTC", balance)
 
-}
+// }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module coinbase-go
+
+go 1.16
+
+require (
+	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
+	github.com/fabioberger/coinbase-go v0.0.0-20160522011833-8328539b18ab
+	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab
+	github.com/stretchr/testify v1.7.0
+)
